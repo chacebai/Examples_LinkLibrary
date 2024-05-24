75b4c211e9f6502135d5714820ed11d53ba97264
chacebai <39848839+chacebai@users.noreply.github.com>
2024-05-24 13:54:00 +0800

    1.add Example02_CalcCPPlib

---
+++ Example02_CalcCPPlib/CalcCPPlib/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#pragma comment(lib, "../Debug/CalcCPPlib_static.lib")
+
+// because of cpp
+int lib_add(int add1, int add2);
+int lib_sub(int sub1, int sub2);
+// end declare
+
+int main()
+{
+	int sum = 0;
+	int sub = 0;
+	sum = lib_add(5, 3);
+	sub = lib_sub(5, 3);
+	// printf("sum=%d, sub=%d\n", sum, sub);
+	std::cout << "sum=" << sum << ", sub=" << sub << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ Example02_CalcCPPlib/CalcCPPlib_static/operations.cpp
@@ -0,0 +1,10 @@
+//#include "operations.h"
+
+int lib_add(int add1, int add2)
+{
+	return add1 + add2;
+}
+int lib_sub(int sub1, int sub2)
+{
+	return sub1 - sub2;
+}
