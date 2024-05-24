d4ddf041797d70a22dec1c385fc4653bb346b9e7
chacebai <39848839+chacebai@users.noreply.github.com>
2024-05-24 13:15:48 +0800

    first commit

---
+++ Example01_C_Calclib_sln/Calclib/main.c
@@ -0,0 +1,13 @@
+#pragma comment(lib, "../Debug/Calclib_static.lib")
+
+int main()
+{
+
+	int sum = 0;
+	int sub = 0;
+	sum = lib_add(5, 3);
+	sub = lib_sub(5, 3);
+	printf("sum=%d, sub=%d\n", sum, sub);
+	system("pause");
+	return 0;
+}
